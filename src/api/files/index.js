7e8650777a43381872f770c38f7624995a4794e9
Kajal lathiya <kajalbodarya77@gmail.com>
2023-01-14 23:24:53 +0530

    add cloudinary url

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -5,24 +5,36 @@ import {
   getProducts,
   writeProducts
 } from "../../lib/fs-tools.js";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const filesRouter = express.Router();
+const cloudinaryUploader = multer({
+  storage: new CloudinaryStorage({
+    cloudinary,  // cloudinary is going to search in .env vars for smt called process.env.CLOUDINARY_URL
+    params: {
+      folder: "epicode/products"
+    }
+  })
+}).single("imageUrl");
 
 filesRouter.post(
   "/:productId/upload",
-  multer().single("imageUrl"),
+  cloudinaryUploader,
   async (req, res, next) => {
     try {
-      const fileName = req.file.originalname;
-      await saveProductsImages(fileName, req.file.buffer);
+      // const fileName = req.file.originalname;
+      // await saveProductsImages(fileName, req.file.buffer);
 
-      const url = `http://localhost:3001/images/${fileName}`;
+      // const url = `http://localhost:3001/images/${fileName}`;
 
+      console.log(req.file);
+      const url = req.file.path;
       const products = await getProducts();
 
       const index = products.findIndex(
         (product) => product._id === req.params.productId
-      ); 
+      );
       if (index !== -1) {
         const oldProduct = products[index];
 
